e65303c84f6f6fc1023375bb55fb1d4295dcdc76
Joe Lanman <joe.lanman@gmail.com>
2012-12-20 16:08:30 +0000

    added spinner

--- public/js/main.js
+++ public/js/main.js
@@ -330,9 +330,12 @@ $(function(){
 				var url = "/api/jobs/search?url=" + autoLoadURL;
 
 				console.log('autoloading ' + url);
+				
+				$('#jobsWrap .loading').show();
 	
 				$.get(url, function(data){
 
+					$('#jobsWrap .loading').hide();
 					drawJobs(data);
 
 				});
@@ -493,6 +496,24 @@ $(function(){
 		
 	});
 
-	search.getJobs();	
+	search.getJobs();
+	
+	var opts = {
+	  lines: 8, // The number of lines to draw
+	  length: 7, // The length of each line
+	  width: 4, // The line thickness
+	  radius: 8, // The radius of the inner circle
+	  corners: 0, // Corner roundness (0..1)
+	  rotate: 0, // The rotation offset
+	  color: '#000', // #rgb or #rrggbb
+	  speed: 1, // Rounds per second
+	  trail: 60, // Afterglow percentage
+	  shadow: false, // Whether to render a shadow
+	  hwaccel: true, // Whether to use hardware acceleration
+	  className: 'spinner', // The CSS class to assign to the spinner
+	  zIndex: 2e9, // The z-index (defaults to 2000000000)
+	};
+	var target = $('#jobsWrap .loading')[0];
+	var spinner = new Spinner(opts).spin(target);
 	
 });
